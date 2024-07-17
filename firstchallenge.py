6fdf1e1f74aa42bd6337502228101dfd4df8d072
matej <matej.basta@email.cz>
2024-07-17 17:14:26 +0200

    adding output file

--- firstchallenge.py
+++ firstchallenge.py
@@ -3,29 +3,30 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file")
+    parser.add_argument("output_file")
     args = parser.parse_args()
 
     try:
         with open(args.input_file, "r") as input_file:
-            total_points = 0
-            for line in input_file:
-                lists = line.strip("\n").split("|")
-                numbers = {}
-                for number in lists[0].split(":")[1].strip().split():
-                    numbers[number] = True        
-                has_points = False
-                points = 0
-                for number in lists[1].strip().split():
-                    if numbers.get(number):
-                        print(number)
-                        if has_points:
-                            points *= 2
-                        else:
-                            points = 1
-                            has_points = True
-                print(points)    
-                total_points += points
-            print(total_points)
+            with open(args.output_file, "w") as output_file:
+                total_points = 0
+                for line in input_file:
+                    lists = line.strip("\n").split("|")
+                    numbers = {}
+                    for number in lists[0].split(":")[1].strip().split():
+                        numbers[number] = True        
+                    has_points = False
+                    points = 0
+                    for number in lists[1].strip().split():
+                        if numbers.get(number):
+                            if has_points:
+                                points *= 2
+                            else:
+                                points = 1
+                                has_points = True
+                    total_points += points
+                print(total_points)
+                output_file.write(str(total_points))
 
     except FileNotFoundError:
         print("File does not exist.")
