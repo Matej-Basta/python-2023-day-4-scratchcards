8d068a22c28f153acded1ad5884b0319530af364
matej <matej.basta@email.cz>
2024-07-17 17:11:52 +0200

    first challenge working

--- firstchallenge.py
+++ firstchallenge.py
@@ -11,17 +11,19 @@ def main():
             for line in input_file:
                 lists = line.strip("\n").split("|")
                 numbers = {}
-                for number in lists[0].split(":")[1].strip().split(" "):
+                for number in lists[0].split(":")[1].strip().split():
                     numbers[number] = True        
                 has_points = False
                 points = 0
-                for number in lists[1].strip().split(" "):
+                for number in lists[1].strip().split():
                     if numbers.get(number):
+                        print(number)
                         if has_points:
                             points *= 2
                         else:
                             points = 1
                             has_points = True
+                print(points)    
                 total_points += points
             print(total_points)
 
